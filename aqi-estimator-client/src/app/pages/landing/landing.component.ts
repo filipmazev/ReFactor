aa472b13dbc9983967e9fb706c740711fe188e9b
filipmazev <56910771+filipmazev@users.noreply.github.com>
2024-11-27 21:39:58 +0100

    X

--- aqi-estimator-client/src/app/pages/landing/landing.component.ts
+++ aqi-estimator-client/src/app/pages/landing/landing.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { ButtonComponent } from '../../components/ui/button/button.component';
 
 @Component({
     selector: 'app-landing',
     standalone: true,
     imports: [
         TranslateModule,
-        ButtonComponent,
     ],
     templateUrl: './landing.component.html',
     styleUrl: './landing.component.scss'
