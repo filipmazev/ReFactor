7dd25bcaecc7281739c9c1351d674e53c2bae05a
filipmazev <56910771+filipmazev@users.noreply.github.com>
2024-11-29 00:45:20 +0100

    environment

--- aqi-estimator.client/src/client/predictor.service.ts
+++ aqi-estimator.client/src/client/predictor.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { take } from "rxjs";
+import { environment } from "../environments/environment";
 
 @Injectable({
     providedIn: 'root'
   })
 export class PredictorService {
-    
+    private readonly BASE_PATH: string = environment.apiPath;
+
     constructor(private httpClient: HttpClient) {
 
     }
@@ -14,7 +16,7 @@ export class PredictorService {
     public predict(data: Blob): Promise<any> {
         return new Promise<any>((resolve, reject) => {
             try {
-                this.httpClient.post("http://localhost:8080/api/aqi/process", data).pipe(take(1))
+                this.httpClient.post(this.BASE_PATH + "/aqi/process", data).pipe(take(1))
                 .subscribe((response: any) => {
                     resolve(response);
                 });
