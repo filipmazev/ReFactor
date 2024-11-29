43d8026515ee50c7488ec03572a9377e754e775b
filipmazev <56910771+filipmazev@users.noreply.github.com>
2024-11-29 21:09:49 +0100

    //

--- aqi-estimator.server/src/main/java/com/codefu/refactor/api/aqiestimatorserver/Application.java
+++ aqi-estimator.server/src/main/java/com/codefu/refactor/api/aqiestimatorserver/Application.java
@@ -1,9 +1,15 @@
 package com.codefu.refactor.api.aqiestimatorserver;
 
+import com.codefu.refactor.api.aqiestimatorserver.imageProcessor.ImagePipelineClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
