ed5a542ab8f903f7e421abfa2702f79b287a6996
filipmazev <56910771+filipmazev@users.noreply.github.com>
2024-11-30 17:31:53 +0100

    deploy

--- aqi-estimator.client/src/environments/environment.prod.ts
+++ aqi-estimator.client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     i18nPath: '/assets/i18n/',
-    apiPath: 'http://localhost:8080/api',
+    apiPath: 'https://leading-tapir-violently.ngrok-free.app/api',
     dbVersion: 2,
 };
